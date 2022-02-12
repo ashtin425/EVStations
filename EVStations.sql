2c496a5a2cd07ead995fd2b0e79efe52e2e6145f
Ashtin <91440736+ashtin425@users.noreply.github.com>
2022-02-12 11:49:36 -0500

    Add files via upload
    initial commit

---
+++ EVStations.sql
@@ -0,0 +1,65 @@
+-- looking at the table
+select *
+from EVChargerProject..ev_stations
+
+-- deleting Washington DC, Ontario, and Puerto Rico from table.
+delete from EVChargerProject..ev_stations
+where state = 'ON' 
+	or state = 'PR' 
+	or state = 'DC'
+
+-- looking at states with the most EV stations
+select distinct State, count(*) as 'EV Stations'
+from EVChargerProject..ev_stations
+group by state
+order by count(*) desc
+
+-- Looking at states with the least amount of EV stations
+select distinct State, count(*) as 'EV Stations'
+from EVChargerProject..ev_stations
+group by state
+order by count(*) asc
+
+-- looking at cities with the most EV stations
+select distinct City, State, count(*) as 'EV Stations'
+from EVChargerProject..ev_stations
+group by City, State
+order by count(*) desc
+
+-- looking at cities with the most public EV Stations
+select distinct City, State, count(*) as 'Public EV Stations'
+from EVChargerProject..ev_stations
+where [Groups With Access Code] = 'Public'
+group by state, City
+order by count(*) desc
+
+-- Looking at states w/ most EV station that are open 24 hours a day
+select distinct State, count(*) as '24 Hour EV Stations'
+from EVChargerProject..ev_stations
+where [Access Days Time] = '24 hours daily' 
+group by state
+order by count(*) desc
+
+-- Looking at the distinct facility types that have the most EV stations
+select distinct [Facility Type], count(*) as 'EV Stations'
+from EVChargerProject..ev_stations
+where [Facility Type] is not null 
+group by [Facility Type]
+order by count(*) desc
+
+--Looking at how many EV Stations having record of opening in the last 10 years
+select distinct State, count(*) as 'EV Stations'
+from EVChargerProject..ev_stations
+where [Facility Type] is not null
+	and [Open Date] between '2012-01-01' and '2022-01-01'
+group by state
+order by count(*) desc
+
+
+
+
+
+
+
+
+
